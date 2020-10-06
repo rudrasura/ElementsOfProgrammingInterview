2765a20448698af76e2028ed2718adf203c213e9
rudrasura <mpai1391@gmail.com>
2020-10-06 17:46:38 +0530

    Arrays :: Changing the file for java/Rearrange.java

--- Arrays/Rearrange_Array_Even_First/java/Rearrange.java
+++ Arrays/Rearrange_Array_Even_First/java/Rearrange.java
@@ -1,23 +1,28 @@
 package arrays;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+/**
+ * Program to reorder arrays' entries so that the even entries appear first
+ * without allocating additional storage.
+ */
 public class Rearrange {
     public static void main(String args[]) {
-        //int a[] = new int[]{3,2,10,4,1,8};
+        int a[] = new int[]{3,2,10,4,1,8};
         rearrangeEvens(a);
     }
 
-    private static void rearrangeEvens(List<String> list) {
-
-        List<String> filtered = list.stream().filter(s -> s.length() > 4).collect(Collectors.toList());
-
+    private static void rearrangeEvens(int[] a) {
+        int e = 0; int o = a.length-1;
+        while(e < o) {
+            if(a[e] % 2 == 0) {
+                e++;
+            } else {
+                int temp = a[e];
+                a[e] = a[o];
+                a[o--] = temp;
+            }
+        }
+        for(int i = 0 ; i < a.length; i++) {
+            System.out.print(a[i]+ " ");
+        }
     }
-
-
-
-
-
 }
-
